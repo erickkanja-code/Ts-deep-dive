2ff3bd0e172ad049389c405fd24baba9bf4ea47f
Erick Kanja <e.kanja@alustudent.com>
2026-02-09 21:35:37 +0300

    Add solved intro to typescript practice problem

---
+++ index2.ts
@@ -0,0 +1,41 @@
+type Address = {
+        street: string,
+        city: string,
+        country: string
+    }
+
+type Person = {
+    name: string,
+    age: number,
+    isStudent: boolean,
+    address?: Address
+}
+
+
+
+
+
+
+let person: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }  
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
+
+let people: Person[] = [person, person2]
